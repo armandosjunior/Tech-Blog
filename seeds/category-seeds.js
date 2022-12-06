b82b55625c33bcb25dae15c027b9155462939494
armandosjunior <sidaarmando@gmail.com>
2022-12-06 11:17:23 -0700

    finished seeds

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -1,7 +1,7 @@
 const { Category } = require('../models');
 
 const categoryData = [
-    { category_name: 'Shirts'},
+    {category_name: 'Shirts'},
     {category_name: 'Shorts'},
     {category_name: 'Music'},
     {category_name: 'Hats'},
